14e316bc60ba923b91ca8ffa923d0e6dddc6de04
jafarislam1001 <jafarisla1001@gmail.com>
2024-05-31 23:07:55 +0600

    Change greeting method

--- project3.java
+++ project3.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
       System.out.println("Hello World");
+
+      greeting("Git");
     }
+
+    public static void greeting(String name){
+      System.out.println("bye bye" + name);
+    }
+      
   }
