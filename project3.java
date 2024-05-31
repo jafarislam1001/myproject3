b6248eaa9b5bd79616f0edc1bfa2c88a80f742f9
jafarislam1001 <jafarisla1001@gmail.com>
2024-06-01 00:19:10 +0600

    Merge from branch B

--- project3.java
+++ project3.java
@@ -2,4 +2,13 @@ public class Main {
     public static void main(String[] args) {
       System.out.println("Hello World");
     }
+
+    public static void greeting(String name){
+      System.out.println("Welcome");
+
+    }
+
+    public static int sqr(int x){
+      return x*x;
+    }
   }
